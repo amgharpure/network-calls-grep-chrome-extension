2fe696b537642da02f8c3051cfa094dc7cd80129
Ameya Gharpure <ameyagharpure@Ameyas-Air.lan>
2021-09-12 18:53:14 -0400

    chore: added comments and function documentation

--- js/panel.js
+++ js/panel.js
@@ -9,17 +9,28 @@ $(function() {
 
 
 function checkRequest(data) {
+  /**
+   * check the network call for the search string and highlight if matched
+   * @param {} data network request object
+   */
+
+  // check if the search string is valid
   let inputText = $userInput.val();
+
 	if (inputText != "" && inputText != null) {
 		if ([200, 201, 304].includes(data.response.status)) {
 			data.getContent(function (content, encoding) {
         let inputText = $userInput.val();
-				let strToSearch = (!!data.request && data.request.url) + content;
+				
+        // search the request in addition to the response
+        let strToSearch = (!!data.request && data.request.url) + content;
         if (strToSearch != undefined && strToSearch.indexOf(inputText) >= 0) {
           let networkCallName = data.request.method + " " + data.request.url;
           let $requestLogElement = $("<div>Found : " + inputText +
           "<br>In request: " + networkCallName + "<br><br></div>");
           let tr = $userContent.prepend($requestLogElement);
+
+          // flash the network call list item if it contains the search string  
           flashElement($requestLogElement);
         }
 			})
@@ -28,5 +39,9 @@ function checkRequest(data) {
 }
 
 function flashElement ($el) {
+  /**
+   * flash an element green for 2 seconds
+   * @param {jQuery} $el The element that you want to flash once
+   */
   $el.addClass('greenHighlight');
 }
